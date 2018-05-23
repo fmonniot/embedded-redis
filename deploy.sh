029fb86cc2eb9b29ec0364e7095a076302e7425e
François Monniot <francoismonniot@gmail.com>
2018-05-22 18:26:18 -0700

    Deploy to bintray via JDK8 instead of 7
    This may fix a problem with the bintrayUpload task

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 test "${TRAVIS_PULL_REQUEST}" == "false" && \
-    test "${TRAVIS_JDK_VERSION}" == "oraclejdk7" && \
+    test "${TRAVIS_JDK_VERSION}" == "oraclejdk8" && \
     test "${TRAVIS_TAG}" != "" && \
     echo Deploying to Bintray && \
     ./gradlew bintrayUpload
